337742fedf0b4c82db93223fda47ad382dacd8f9
Anton <168501764+aduarte6@users.noreply.github.com>
2024-12-15 19:12:28 -0600

    fixed file name socialmedia

--- components/HomeSocial.jsx
+++ components/HomeSocial.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import styles from '@/assets/styles/HomeSocial.module.css';
-import images from '@/socialMedia.json';
+import images from '@/socialmedia.json';
 
 const HomeSocial = () => {
 
