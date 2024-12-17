018030f709f3bc280709f602e1ba6d890ae0fe5d
Anton <168501764+aduarte6@users.noreply.github.com>
2024-12-16 19:22:15 -0600

    chnaged to @/

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -3,7 +3,7 @@
 import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-import outputs from "../../amplify_outputs.json";
+import outputs from "@/amplify_outputs.json";
 import { fetchUserAttributes } from "aws-amplify/auth";
 import { useEffect, useState } from "react";
 
