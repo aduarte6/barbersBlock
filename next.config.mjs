95511252dccba1c405e3e6389214b81b722eba63
Anton <168501764+aduarte6@users.noreply.github.com>
2024-12-16 22:34:30 -0600

    next congif

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.mjs
+export default {
+    target: 'serverless',  // This ensures SSR is supported on AWS Amplify
+    experimental: {
+        outputStandalone: true, // Ensures standalone output for SSR
+    },
+    reactStrictMode: true,  // Optional: Helps with React optimizations
+};
