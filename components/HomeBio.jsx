1833d6397d5fde959390d5b9b1c4cc52ff3c9f87
Anton <168501764+aduarte6@users.noreply.github.com>
2024-12-18 17:09:33 -0600

    babrershop sign

--- components/HomeBio.jsx
+++ components/HomeBio.jsx
@@ -6,13 +6,13 @@ import Image from 'next/image'
 
 const HomeBio = () => {
   return (
-      <section className=" relative max-w-screen-lg m-auto justify-center">
-          <div className="w-full flex flex-col gap-4 mt-8 px-4">
+      <section className=" relative max-w-screen-xl m-auto justify-center">
+          <div className="w-full flex flex-col gap-4 mt-8 px-8">
               <div className="max-w-[3em] text-[clamp(4rem,5vw,5em)] font-[900] text-chicagoOrange leading-none">
 
                   <p>Chicago&apos;s Newest Barbershop</p>
               </div>
-              <div className='font-[200] text-[1rem] max-w-[20em]'>
+              <div className='font-[200] text-[1.2rem] max-w-[20em]'>
                   <p>
                       Barbers Block is dedicated to serving our community with a blend of style, quality,
                       and authenticity. Whether you’re looking for a fresh cut, a classic style, or something
@@ -31,7 +31,7 @@ const HomeBio = () => {
               </div>
 
           </div>
-          <div className="w-full absolute right-[0] top-0 max-w-[25em]">
+          <div className="w-full absolute right-[0] top-0 max-w-[clamp(25em,50vw,50em)]">
               <Image  className="w-full max-w-[50em] blur-[6px] opacity-[0.4]" src="/images/home/chicagoout.webp" alt="https://stock.adobe.com/images/chicago/431255482?prev_url=detail" width={100} height={800} />
 
           </div>
--- components/testimonials/Testimonials.tsx
+++ components/testimonials/Testimonials.tsx
@@ -169,18 +169,71 @@ const Testimonials: FC<TestimonialsProps> = ({ position, testimonial, handleMove
             />
             <h3
                 className={`text-base sm:text-xl ${
-                    isActive ? "text-white" : "text-white"
+                    isActive ? "text-white" : "text-white/60"
                 }`}
             >
                 &quot;{testimonial.testimonial}&quot;
             </h3>
-            <p
-                className={`absolute bottom-8 left-8 right-8 mt-2 text-sm italic ${
-                    isActive ? "text-white/60" : "text-white/40"
-                }`}
-            >
-                - {testimonial.by}
-            </p>
+
+
+            <div className="w-full justify-around flex flex-row absolute   text-sm italic items-end">
+                <div className={"w-1/2"}>
+                    <p
+                    className={` ${
+                        isActive ? "text-white/60" : "text-white/40"
+                    }`}
+                >
+                    - {testimonial.by}
+                    </p>
+                </div>
+                <div className={"w-1/4 items-end"}>
+
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 2000 2000"
+
+                        >
+                            <rect  />
+                            <path  className={` ${
+                                isActive ? "fill-white/20" : "fill-gray"
+                            }`}
+                                d="M873.6,1660.7c104.7,0,190.3-81.8,196.5-184.9h-392.9c6.2,103.2,91.8,184.9,196.5,184.9Z"/>
+                            <path
+                                className={` ${
+                                    isActive ? "fill-white/20" : "fill-gray"
+                                }`}
+                                d="M873.6,354.9c-104.7,0-190.3,81.7-196.5,184.9h392.9c-6.2-103.2-91.8-184.9-196.5-184.9ZM798.9,510.9h-65.4c4.2-70.9,63.1-127.2,135.1-127.2s21.1,1.2,31,3.6c-53.6,14.7-94.4,63.5-100.8,123.6Z"/>
+                            <path
+                                className={` ${
+                                    isActive ? "fill-white/20" : "fill-gray"
+                                }`}
+                                d="M697.1,873.5l148.1-148.1c11.2-11.2,7.4-20.3-8.4-20.3h-131.3c-15.8,0-28.7,12.9-28.7,28.7v131.3c0,15.8,9.1,19.6,20.3,8.4Z"/>
+                            <path
+                                className={` ${
+                                    isActive ? "fill-white" : "fill-white"
+                                }`}
+                                d="M676.8,1246.8c0,15.8,9.1,19.5,20.2,8.4l328.1-329.7c9.9-9.9,22.5-22.7,30.1-30.5.9-.9,1.8-1.8,2.7-2.7,6.2-6.3,6.7-7,1.8-2.1,6.1-8.2,10.8-25,10.8-39.5v-117c0-15.8-.7-28.7-1.5-28.7s-10.6,9.1-21.8,20.3l-350.1,350.1c-11.2,11.2-20.3,33.2-20.3,49v122.4Z"/>
+                            <path
+                                className={` ${
+                                    isActive ? "fill-white/20" : "fill-gray"
+                                }`}
+                                d="M1050,1102.3l-189.4,188.1c-11.2,11.1-7.5,20.2,8.3,20.2h172.7c15.8,0,28.7-12.9,28.7-28.7v-171.1c0-15.8-9.2-19.6-20.4-8.5Z"/>
+                            <path
+                                className={` ${
+                                    isActive ? "fill-white/20" : "fill-gray"
+                                }`}
+                                d="M1105.7,578.1h-469.5c-10.2,0-18.5,8.3-18.5,18.5v51.8c0,10.2,8.3,18.5,18.5,18.5h469.5c10.2,0,18.5-8.3,18.5-18.5v-51.8c0-10.2-8.3-18.5-18.5-18.5Z"/>
+                            <path
+                                className={` ${
+                                    isActive ? "fill-white/20" : "fill-grey"
+                                }`}
+                                d="M1105.7,1348.8h-469.5c-10.2,0-18.5,8.3-18.5,18.5v51.8c0,10.2,8.3,18.5,18.5,18.5h469.5c10.2,0,18.5-8.3,18.5-18.5v-51.8c0-10.2-8.3-18.5-18.5-18.5Z"/>
+                        </svg>
+                    </div>
+
+            </div>
+
+
         </motion.div>
     );
 };
@@ -198,14 +251,14 @@ const StaggerTestimonials: FC = () => {
                 const firstEl = copy.shift();
 
                 if (!firstEl) return;
-                copy.push({ ...firstEl, tempId: Math.random() });
+                copy.push({...firstEl, tempId: Math.random()});
             }
         } else {
             for (let i = position; i < 0; i++) {
                 const lastEl = copy.pop();
 
                 if (!lastEl) return;
-                copy.unshift({ ...lastEl, tempId: Math.random() });
+                copy.unshift({...lastEl, tempId: Math.random()});
             }
         }
 
@@ -213,12 +266,12 @@ const StaggerTestimonials: FC = () => {
     };
 
     useEffect(() => {
-        const { matches } = window.matchMedia("(min-width: 640px)");
+        const {matches} = window.matchMedia("(min-width: 640px)");
 
         setCardSize(matches ? CARD_SIZE_LG : CARD_SIZE_SM);
 
         const handleSetCardSize = () => {
-            const { matches } = window.matchMedia("(min-width: 640px)");
+            const {matches} = window.matchMedia("(min-width: 640px)");
             setCardSize(matches ? CARD_SIZE_LG : CARD_SIZE_SM);
         };
 
